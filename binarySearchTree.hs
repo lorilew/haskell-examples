905eee4a488053b0f85af82b40d0ab03a153f7c0
Lauren Lewis <lorilew123@gmail.com>
2016-10-23 17:53:33 +0100

    Made tree an instance of Functor

--- binarySearchTree.hs
+++ binarySearchTree.hs
@@ -1,6 +1,10 @@
 data Tree a = EmptyTree | Node a (Tree a) (Tree a)
   deriving (Show, Read, Eq)
 
+instance Functor Tree where
+  fmap f EmptyTree = EmptyTree
+  fmap f (Node x leftsub rightsub) = Node (f x) (fmap f leftsub) (fmap f rightsub)
+
 singleton x = Node x EmptyTree EmptyTree
 
 treeInsert x EmptyTree = singleton x
