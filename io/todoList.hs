c0bfd06b17d5891936d980284de4008a6ef06ea2
Lauren Lewis <lorilew123@gmail.com>
2016-10-24 14:26:36 +0100

    command line arguments

--- io/todoList.hs
+++ io/todoList.hs
@@ -1,21 +1,55 @@
 import System.IO
 import System.Directory
+import System.Environment
 import Data.List
 
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [("add", add)
+           , ("view", view)
+           , ("remove", remove)
+           , ("bump", bump)
+           ]
+
 main = do
-  handle <- openFile "todo.txt" ReadMode
+   (command:args) <- getArgs
+   let (Just action) = lookup command dispatch
+   action args
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+   contents <- readFile fileName
+   let todoTasks = lines contents
+       numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+   putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+   handle <- openFile fileName ReadMode
+   (tempName, tempHandle) <- openTempFile "." "temp"
+   contents <- hGetContents handle
+   let number = read numberString
+       todoTasks = lines contents
+       newTodoItems = delete (todoTasks !! number) todoTasks
+   hPutStr tempHandle $ unlines newTodoItems
+   hClose handle
+   hClose tempHandle
+   removeFile fileName
+   renameFile tempName fileName
+
+bump :: [String] -> IO ()
+bump [fileName, numberString] = do
+  handle <- openFile fileName ReadMode
   (tempName, tempHandle) <- openTempFile "." "temp"
   contents <- hGetContents handle
-  let todoTasks = lines contents
-      numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
-  putStrLn "These are your todo items:"
-  putStr $ unlines numberedTasks
-  putStrLn "Which do you want to delete?"
-  numberString <- getLine
   let number = read numberString
-      newTodoItems = delete (todoTasks !! number) todoTasks
+      todoTasks = lines contents
+      bumpTask = todoTasks !! number
+      newTodoItems = bumpTask : (delete (todoTasks !! number) todoTasks)
   hPutStr tempHandle $ unlines newTodoItems
   hClose handle
   hClose tempHandle
-  removeFile "todo.txt"
-  renameFile tempName "todo.txt"
+  removeFile fileName
+  renameFile tempName fileName
